ccb3bab446883b15a058d54dace89274f39ab5e0
Sebastian HdezMont <sebashermon003@gmail.com>
2026-03-09 12:22:59 -0600

    feat(DatabaseConnection): default database connection

--- app/backend/src/app.module.ts
+++ app/backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Here goes the entities
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.HOST ?? 'localhost',
+      port: parseInt(process.env.DATABASE_PORT!) ?? 1433,
+      username: process.env.DATABASE_USER ?? 'sa',
+      password: process.env.DATABASE_PASSWORD ?? 'YourStrong!Passw0rd',
+      database: process.env.DATABASE ?? 'test',
+      entities: [],
+      // synchronize: true,
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
